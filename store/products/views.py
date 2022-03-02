e6f48507294c3364dc0b316c8e3d267017c24d43
meshok <qmeshokp@mail.ru>
2022-03-02 21:30:33 +0300

    Добавил продуктов в категорию пицца

--- store/products/views.py
+++ store/products/views.py
@@ -14,16 +14,3 @@ def products(request):
     }
     return render(request, 'products/products.html', context)
 
-
-def test_context(request):
-    context = {
-        'title': 'Store',
-        'header': 'Добро пожаловать',
-        'username': 'Хер с горы',
-        'products': [
-            {'name': 'Кофта', 'price': 300},
-            {'name': 'Штаны', 'price': 400},
-            {'name': 'Трусы', 'price': 200},
-        ]
-    }
-    return render(request, 'products/test_context.html', context)
