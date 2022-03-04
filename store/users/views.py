403a42931839e0234fa58b533bab67cebc810d3e
meshok <qmeshokp@mail.ru>
2022-03-04 16:24:32 +0300

    отображение ошибок при регистрации и авторизации

--- store/users/views.py
+++ store/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
@@ -24,7 +24,6 @@ def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
-            form.save()
             return HttpResponseRedirect(reverse('users:login'))
         # else:
         #     print(form.errors)
@@ -44,3 +43,8 @@ def profile(request):
         form = UserProfileForm(instance=request.user)
     context = {'form': form}
     return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
